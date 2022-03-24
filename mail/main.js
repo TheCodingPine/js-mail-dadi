eff74aaffe48e47bf9bd786fbb53cb39a4f13af1
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-24 15:33:31 +0100

    break if true, buggato if false

--- mail/main.js
+++ mail/main.js
@@ -6,12 +6,13 @@ let input = prompt ("Inserisci la tua e-mail")
 console.log("Tentativo di accesso da parte di", input);
 
 //input è nella array utentiValidi?
-let conferma;
+let conferma = false;
 
 for (let filtro=0; filtro < utentiValidi.length; filtro++) {
     if (utentiValidi[filtro] == input) {
         conferma=true;
-        alert ("Benvenuto", utentiValidi[1]);
+        alert ("Benvenuto", utentiValidi[filtro]);
         console.log(utentiValidi[filtro],"è abilitato");
-    } else {alert ("La mail inserita non  valida");}
+        break;
+    } else {alert ("La mail inserita non é valida");}
 }
