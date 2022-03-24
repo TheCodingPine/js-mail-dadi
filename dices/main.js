1acb13317861ee93e6e7cb1751e4dbc36a9a76ae
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-24 17:05:17 +0100

    final commit

--- dices/main.js
+++ dices/main.js
@@ -1,7 +1,15 @@
 let user = prompt("Inserisci il tuo nome per giocare!");
 
-//generiamo
+//generiamo il tiro del player
+let userDice = Math.floor(Math.random() * 6) + 1;
+console.log("Tiro utente", userDice);
+//generiamo il tiro del pc
+let iaDice = Math.floor(Math.random() * 6) + 1;
+console.log("Tiro computer", iaDice);
 
-
-
-Math.floor(Math.random() * 10) + 1;
+if (userDice > iaDice) {
+    console.log(userDice, "è più alto di", iaDice,"!", user, ", hai vinto!");
+} else if (userDice < iaDice) {
+    console.log(userDice, "è minore di", iaDice,"! Peccato", user, ", hai perso!");
+}else {console.log(userDice, "contro", iaDice,"!", user, ", è un pareggio");
+    }
