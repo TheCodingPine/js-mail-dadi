04db03108887e5db0bc69ca69024ee69436f941c
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-24 15:11:51 +0100

    mail array

--- mail/main.js
+++ mail/main.js
@@ -1,2 +1,16 @@
 //la mia array di email
 const utentiValidi = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
+console.log("Carico lista utenti autorizzati");
+
+let input = prompt ("Inserisci la tua e-mail")
+console.log("Tentativo di accesso da parte di", input);
+
+//input è nella array utentiValidi?
+let conferma;
+
+for (let filtro=0; filtro < utentiValidi.length; filtro++) {
+    if (utentiValidi[filtro] == input) {conferma=true;
+    }
+}
+
+alert ("La mail inserita non  valida");
