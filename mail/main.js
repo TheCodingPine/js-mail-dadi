bf81248ebee0d5ce6c76e061355fcdafa2684959
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-24 15:45:55 +0100

    decorazioni & print

--- mail/main.js
+++ mail/main.js
@@ -6,19 +6,22 @@ let input = prompt ("Inserisci la tua e-mail")
 console.log("Tentativo di accesso da parte di", input);
 
 //input è nella array utentiValidi?
-let conferma = false;
+//let conferma = false;
 //dichiaro ora il filtro, così posso usarlo in tutto il codice
 let filtro=0;
 
 for (let filtro=0; filtro < utentiValidi.length; filtro++) {
     if (utentiValidi[filtro] == input) {
-        conferma=true;
+        //conferma=true;
         alert ("Benvenuto", utentiValidi[filtro]);
         console.log(utentiValidi[filtro],"è abilitato");
+        document.getElementById("output").innerHTML = utentiValidi[filtro];
         filtro = 0; //resetto il filtro per evitare problemi
-        break;
+        break; //uscita anticipata dal loop
     } else if (filtro == utentiValidi.length-1) { //e solo se quindi ha fatto tutto il giro dell'array
+        console.log(input,"non è abilitato");
         alert ("La mail inserita non é valida");
-        break;
+        filtro = 0; // resetto per nuovi tentativi
+        break; //uscita anticipata dal loop per evitare enne messaggi di errore
         }
 }
