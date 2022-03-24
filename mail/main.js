8bda3b4973f349eb1fc88a861141506408a3f4b7
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-24 15:38:52 +0100

    fixed!

--- mail/main.js
+++ mail/main.js
@@ -7,12 +7,18 @@ console.log("Tentativo di accesso da parte di", input);
 
 //input è nella array utentiValidi?
 let conferma = false;
+//dichiaro ora il filtro, così posso usarlo in tutto il codice
+let filtro=0;
 
 for (let filtro=0; filtro < utentiValidi.length; filtro++) {
     if (utentiValidi[filtro] == input) {
         conferma=true;
         alert ("Benvenuto", utentiValidi[filtro]);
         console.log(utentiValidi[filtro],"è abilitato");
+        filtro = 0; //resetto il filtro per evitare problemi
         break;
-    } else {alert ("La mail inserita non é valida");}
+    } else if (filtro == utentiValidi.length-1) { //e solo se quindi ha fatto tutto il giro dell'array
+        alert ("La mail inserita non é valida");
+        break;
+        }
 }
